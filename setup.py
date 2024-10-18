750df789054482f83a73421ce224bfff6e7b941e
Andrew Freeman <andrewfreeman234@gmail.com>
2024-10-17 21:21:30 -0600

    feat: Fix for windows

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 # fmt: off
 from setuptools import setup
 
+packages = ["gym_gridworlds"]
+install_requires = [
+    "gymnasium",
+    "pygame"
+]
+entry_points = {
+    "gymnasium.envs": ["Gym-Gridworlds=gym_gridworlds.gym:register_envs"]
+}
+
+
+
+
 setup(
     name="Gym-Gridworlds",
     version="1.0",
